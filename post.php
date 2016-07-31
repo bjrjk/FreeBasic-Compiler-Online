bb1440a1477c4a68cb5b159edbe761453b38c1e6
Jack Ren <bjrjk@qq.com>
2016-07-31 14:22:20 +0800

    Commit New

--- post.php
+++ post.php
@@ -35,7 +35,7 @@ function Make_Text($dir_path,$pass,$code){
 
 $code=$_POST["code"];
 $pass=randstr();
-if(stripos($code,"shell")!=false||stripos($code,"open")!=false){
+if(stripos($code,"shell")!=false||stripos($code,"open")!=false||stripos($code,"exec")!=false){
 	Make_Text("shell/failed/",$pass,$code);
 	die("Don't do bad things!Recorded!");
 }
