59ff44e24a44efcd35a64e70daa1550886678198
Jack Ren <bjrjk@qq.com>
2016-08-01 18:35:26 +0800

    Commit New

---
+++ clear.php
@@ -0,0 +1,3 @@
+<?php
+//clear.php
+?>
--- post.php
+++ post.php
@@ -61,4 +61,4 @@ unlink("app/".$pass);
 echo "<title>FreeBasic Compier Online--Result</title><div align=\"center\"><h2>FreeBasic Compier Online--Result</h2>";
 echo "<p>Code:</p><textarea rows=10 cols=50>$code</textarea><p>Result:</p><textarea rows=10 cols=50>$result</textarea>";
 echo "</div>";
-?>?>?>
+?>
