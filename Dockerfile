f0789c0631ef763fc0ab7844151191686e204df2
Jack Ren <bjrjk@qq.com>
2016-08-15 08:27:43 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apt-get install python g++ gcc make build-essential tar wget python -y
 RUN wget http://heanet.dl.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.05.0-linux-x86_64.tar.gz
 RUN tar zxvf FreeBASIC-1.05.0-linux-x86_64.tar.gz
 WORKDIR /root/FreeBASIC-1.05.0-linux-x86_64
-RUN ./install.sh -i /
+RUN ./install.sh -i
 WORKDIR /var/www/html/
 RUN rm -rf *
 COPY ./ /var/www/html/
--- Main.py
+++ Main.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 import os,time
 import os.path
+print "Starting Apache..."
 os.system("/etc/init.d/apache2 start")
 absolute_path=""
 code_dir=absolute_path+"code/"
 app_dir=absolute_path+"app/"
 time_count=0
 
+print "Starting Loop..."
 while 1:
 	for parent,dirnames,filenames in os.walk(code_dir):
 		for filename in filenames:
 			if filename[-3:]=="php":
 				continue
-			command_line="fbc -x %s %s"%(app_dir+filename[:-4],code_dir+filename)
+			command_line="/usr/local/bin/fbc -x %s %s"%(app_dir+filename[:-4],code_dir+filename)
 			print command_line
 			os.system(command_line)
 			if os.path.exists(code_dir+filename):
@@ -20,6 +22,8 @@ while 1:
 				os.remove(code_dir+filename)
 	time.sleep(0.5)
 	time_count+=1
+	print "Loop "+str(time_count)+" finsished!"
 	if time_count>=144000:
 		time.count=0
 		os.system("rm error/*.txt timeup/*.txt")
+		print "Clear Log Finished!"
